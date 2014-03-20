7c35c778c5c91101c1d8154a1a0601d16ee11503
vesylivskyy <vesylivskyy@gmail.com>
2014-03-20 16:11:38 +0000

    Include tab panel

--- popup.js
+++ popup.js
@@ -19,19 +19,6 @@ function showOpenTab() {
 }
 
 // assign onclick event handlers to tab buttons elements
-//document.getElementById('open_btn').onclick = showOpenTab;
-//document.getElementById('save_btn').onclick = showSaveTab;
-
-
-
-	
-/*
-document.getElementById('save_btn').onclick = function() {
-	document.getElementById('save_btn').setAttribute('class', 'selected'); 
-};
-*/
-
-
 document.getElementById('open_btn').onclick = function() {
 	document.getElementById('open_btn').setAttribute('class', 'selected'); 
 	if(document.getElementById('save_btn').getAttribute("class") == "selected")	{
