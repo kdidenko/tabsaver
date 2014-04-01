4b291ace194e96b0c19e6aa98a98bd5dd03ac1da
kdidenko <kdidenko@gmail.com>
2014-04-01 08:47:19 +0000

    Google Analytics code upgraded

--- tracking.js
+++ tracking.js
@@ -1,4 +1,4 @@
-(function(i, s, o, g, r, a, m) {
+/*(function(i, s, o, g, r, a, m) {
 	i['GoogleAnalyticsObject'] = r;
 	i[r] = i[r] || function() {
 		(i[r].q = i[r].q || []).push(arguments)
@@ -10,4 +10,14 @@
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-2261061-4', 'ito-global.com');
-ga('send', 'pageview');
+ga('send', 'pageview');*/
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-49575781-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
