281c82210355d047eb9a71df16ea0097760a0bac
itodemo <demo@ito-global.com>
2017-10-05 17:36:29 +0100

    basic JSHint linting was integrated into the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,10 @@
  *
  */
 
+/** global includes */
+
+const stylish = require('jshint-stylish'); // let's do it with style ;)
+
 /**
  * Grunt tasks export
  * @param {Object} grunt itself
@@ -11,13 +15,26 @@ module.exports = function (grunt) {
 
     /**
      * Main Grunt configuration initialization function
+     * @param {Object} grunt config
      */
     grunt.initConfig({
 
         // read the project details and settings
-        package: grunt.file.readJSON('package.json')
+        package: grunt.file.readJSON('package.json'),
 
-        // all configuration goes here
+        /** all Grunt configuration goes here */
+
+        /** JSHint task configuration */
+        jshint: {
+            /** linting options */
+            options: {
+                reporter: stylish,
+                esversion: 6
+            },
+
+            /** lint the Gruntfile and all JavaScripts from extension */
+            build: ['Gruntfile.js', 'src/**/*.js']
+        }
 
     });
 
