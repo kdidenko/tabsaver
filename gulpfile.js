d698b35b9559d95a7f93d028b5b0ef94ef809a64
itodemo <demo@ito-global.com>
2017-10-18 17:57:21 +0100

    initial GulpJS automation environment commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * @file gulpfile.js
+ * @description Project testing, building and deployment automation script
+ * 		for GulpJS task runner.
+ */
+
+// initial STUB
+console.log('GulpJS script STUB');
